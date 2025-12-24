3872ce536be10a9735474040ec89b08fe844dc70
eyewave <eyewave@noreply.codeberg.org>
2025-12-24 01:34:27 +0100

    fresh start

--- src/routes/examples/ImageKnob.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { ImageKnob } from '$lib';
-	import imageSource from '../assets/PurpleKnob2.webp';
-
-	let value = $state(0.0);
-</script>
-
-<ImageKnob bind:value src={imageSource} width={90} height={90} />
