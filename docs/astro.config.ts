d10409f05c91e0f72405c2a6a5e8dd01506fbfa2
eyewave <eyewave@noreply.codeberg.org>
2026-02-17 03:14:09 +0100

    fix deploy

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -5,7 +5,7 @@ const prod = (process.env.NODE_ENV = 'production');
 
 // https://astro.build/config
 export default defineConfig({
-	base: prod ? 'https://eye-wave.github.io/web-knobs/' : undefined,
+	base: prod ? '/web-knobs' : undefined,
 	integrations: [
 		svelte({
 			preprocess: vitePreprocess()
